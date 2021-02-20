9b7bee05a55c9361b42847b1c19e0db9ae617d44
Muhammad Rizqi <muhammad-rizqi@live.com>
2021-02-21 06:25:57 +0700

    add scan qr on topup

--- src/screens/Cashier/TopUpScreen.js
+++ src/screens/Cashier/TopUpScreen.js
@@ -1,8 +1,10 @@
+/* eslint-disable react-native/no-inline-styles */
 import {
   Body,
   Button,
   Container,
   Content,
+  Fab,
   Form,
   H3,
   Header,
@@ -19,7 +21,8 @@ import {
   View,
 } from 'native-base';
 import React, {useState} from 'react';
-import {ToastAndroid} from 'react-native';
+import {Modal, ToastAndroid} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 import {getMemberByCode, topUpServices} from '../../services/CashierServices';
 import {styles} from '../../styles/MainStyles';
 
@@ -29,13 +32,14 @@ const TopUpScreen = ({navigation}) => {
   const [topup, setTopup] = useState(0);
   const [memberLoading, setMemberLoading] = useState(false);
   const [member, setMember] = useState(null);
+  const [scan, setScan] = useState(false);
 
-  const onSubmitCode = () => {
-    if (code === '' || code === `${0}`) {
+  const onSubmitCode = (idMember) => {
+    if (idMember === '' || idMember === `${0}`) {
       ToastAndroid.show('Isi dengan benar', ToastAndroid.LONG);
     } else {
       setMemberLoading(true);
-      getMemberByCode(code)
+      getMemberByCode(idMember)
         .then((result) => {
           setMember(result.data.data);
         })
@@ -44,7 +48,10 @@ const TopUpScreen = ({navigation}) => {
           console.log(err);
           console.log(err.response);
         })
-        .finally(() => setMemberLoading(false));
+        .finally(() => {
+          setScan(false);
+          setMemberLoading(false);
+        });
     }
   };
 
@@ -67,6 +74,11 @@ const TopUpScreen = ({navigation}) => {
     }
   };
 
+  const onReadSuccess = ({data}) => {
+    setCode(data);
+    onSubmitCode(data);
+  };
+
   return (
     <Container>
       <Header>
@@ -80,6 +92,27 @@ const TopUpScreen = ({navigation}) => {
         </Body>
         <Right />
       </Header>
+      <Modal visible={scan}>
+        <QRCodeScanner
+          vibrate
+          showMarker
+          onRead={onReadSuccess}
+          reactivate
+          reactivateTimeout={5000}
+        />
+        <Fab
+          style={styles.backgroundPrimary}
+          position="bottomLeft"
+          onPress={() => {
+            setScan(false);
+          }}>
+          <Icon name="arrow-back" />
+        </Fab>
+        {memberLoading && <Spinner />}
+        <H3 style={{color: 'white', textAlign: 'center', margin: 24}}>
+          Scan Member
+        </H3>
+      </Modal>
       <Content style={styles.padding16}>
         <H3 style={styles.marginV16}>Masukkan data topup </H3>
         <Form>
@@ -100,25 +133,31 @@ const TopUpScreen = ({navigation}) => {
                       {member[0].alamat}
                     </Text>
                   </Body>
+                  <Right>
+                    <Button danger transparent onPress={() => setMember(null)}>
+                      <Icon name="trash" />
+                    </Button>
+                  </Right>
                 </ListItem>
               </>
             )
           )}
           <View style={styles.marginV8}>
             <Text note>ID Member</Text>
-            <Item regular>
+            <Item regular style={styles.radius5}>
               <Input
                 placeholder="Masukkan ID Member"
                 value={`${code}`}
                 onChangeText={setCode}
-                onBlur={onSubmitCode}
-                onSubmitEditing={onSubmitCode}
+                onBlur={() => onSubmitCode(code)}
+                onSubmitEditing={() => onSubmitCode(code)}
               />
+              <Icon name="qr-code-outline" onPress={() => setScan(true)} />
             </Item>
           </View>
           <View style={styles.marginV8}>
             <Text note>Jumlah TopUp</Text>
-            <Item regular>
+            <Item regular style={styles.radius5}>
               <Input
                 placeholder="Masukkan Jumlah Uang"
                 value={`${topup}`}
